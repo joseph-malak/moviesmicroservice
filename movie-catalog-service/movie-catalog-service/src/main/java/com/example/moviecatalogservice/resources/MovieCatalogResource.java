4a073b91dda49329c090fca26b24f3cf68693d99
joseph-malak <jmalaks247@gmail.com>
2022-03-10 11:31:12 -0500

    commiting changes to catalog,info,ratings, and creation of discovery service

--- movie-catalog-service/movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,11 +4,16 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movies;
 import com.example.moviecatalogservice.models.Rating;
+import com.example.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
+import java.nio.file.WatchEvent;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -18,24 +23,37 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;//this will tell spring to look for a bean that produces resttemplate, excecutes it then injects it
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+
 
         RestTemplate restTemplate = new RestTemplate(); //has methods
         // get all rated movie IDs /
         // this list should come from the api
-        List<Rating> ratings = Arrays.asList(
-
-                new Rating("1234", 5),
-                new Rating("5678", 6)
-        );
+        List<Rating> ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
         //looping
         //hard code return
-        return ratings.stream().map(rating ->{
-                //calls are synchronous but you can make them asynchronous
-                   Movies movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId() /*get that specific id from each movie*/, Movies.class);// getForObject creates an instance of a class and give properties to it, gives fully formed obj
+        return ratings.getUserRating().stream().map(rating ->{
+
+                    // For each one, call movie info service and get details
+                   Movies movie = restTemplate.getForObject("http://localhost:8083/movies/" + rating.getMovieId() /*get that specific id from each movie*/, Movies.class);// getForObject creates an instance of a class and give properties to it, gives fully formed obj
 
                    return new CatalogItem(movie.getName(), "things happen" , rating.getRating());
+
+//                  Movies movie = webClientBuilder.build()
+//                           .get()
+//                           .uri("http://localhost:8083/movies/" + rating.getMovieId())
+//                           .retrieve()
+//                           .bodyToMono(Movies.class);
+//                            .block();
+
 
         })
                 .collect(Collectors.toList());
@@ -43,7 +61,7 @@ public class MovieCatalogResource {
 
 
 
-        // For each one, call movie info service and get details
+
 
         // Put them all together
 
--- ratings-data-service/src/main/java/com/example/ratingsdataservice/resource/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/resource/RatingsResource.java
@@ -2,9 +2,13 @@ package com.example.ratingsdataservice.resource;
 
 
 import com.example.ratingsdataservice.model.Rating;
+import com.example.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -13,6 +17,18 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable ("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getUserRating(@PathVariable ("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+
+                new Rating("1234", 5),
+                new Rating("5678", 6)
+        );
+        UserRating userRating = new UserRating;
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 
 }
